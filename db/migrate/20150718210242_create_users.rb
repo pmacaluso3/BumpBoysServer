9771ec98e498cd6d768376995dc6749a9c3d9da5
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-07-18 19:59:05 -0500

    adding user/contacts migrations and seeds

--- db/migrate/20150718210242_create_users.rb
+++ db/migrate/20150718210242_create_users.rb
@@ -1,9 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :udid
       t.string :first_name
       t.string :last_name
       t.string :phone_number
+      t.string :image_url
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+bo = User.create!(udid: nil,
+  first_name: "Bo",
+  last_name: "Guthrie"
+  phone_number: "2058070850",
+  image_url: "https://pbs.twimg.com/profile_images/443395868134088704/enQ-y1aY.jpeg")
+
+nick = User.create!(udid: nil,
+  first_name: "Nick",
+  last_name: "Siefken"
+  phone_number: "3096455208",
+  image_url: "https://pbs.twimg.com/profile_images/3406647874/55e6e2b3a2cd929d478213bca7ea346a.jpeg")
+
+
+bo_contact = Contact.create! (udid: nil,
+  first_name: "Bo",
+  last_name: "Guthrie"
+  phone_number: "2058070850")
+
+nick_contact = Contact.create! (udid: nil,
+  first_name: "Nick",
+  last_name: "Siefken"
+  phone_number: "3096455208")
+
+bo.contacts << nick
+nick.contacts << bo
