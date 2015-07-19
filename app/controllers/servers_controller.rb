0626e605d8d1b86e34c21fae82c85cae6635ccaa
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-07-19 14:40:39 -0500

    sending apn to all new friends in radius is written, might even work

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,3 +1,5 @@
+require 'houston'
+
 class ServersController < ApplicationController
   def run
     feet_in_one_lat = 364402.01
@@ -51,21 +53,171 @@ class ServersController < ApplicationController
         User.find_by(token: user_token).nearby_friends_images = this_users_nearby_friends_images.join(",")
         new_friends_in_radius = this_users_nearby_friends_tokens - this_users_nearby_friends_tokens_previous
         new_friends_in_radius.each do |friend_token|
-
+          friend = User.find_by(token: friend_token)
+          send_apn(user_token, friend.first_name, friend.last_name)
         end
       end
 
       # for each user, prepare the list of people this user should receive push notifications about
-      @relationships.each do |user_token, mutual_contacts_list|
-        mutual_contacts_list.each do |mutual_contact_token, distance|
-          if distance < 1000
-
-          end
-        end
-      end
+      # @relationships.each do |user_token, mutual_contacts_list|
+      #   mutual_contacts_list.each do |mutual_contact_token, distance|
+      #     if distance < 1000
+      #     end
+      #   end
+      # end
 
 
     # end
     render 'runs/info'
+  end
+
+  private
+  def send_apn(recipient_token, first_name, last_name)
+    APN = Houston::Client.development
+    APN.certificate = "Bag Attributes
+      friendlyName: Apple Development IOS Push Services: com.org.Isaac
+      localKeyID: F6 71 9F C3 69 9E F7 28 37 D0 62 73 0A BA 71 24 63 0E 71 DF
+  subject=/UID=com.org.Isaac/CN=Apple Development IOS Push Services: com.org.Isaac/OU=328SKT5YWC/C=US
+  issuer=/C=US/O=Apple Inc./OU=Apple Worldwide Developer Relations/CN=Apple Worldwide Developer Relations Certification Authority
+  -----BEGIN CERTIFICATE-----
+  MIIFfjCCBGagAwIBAgIICORl7oRebFEwDQYJKoZIhvcNAQEFBQAwgZYxCzAJBgNV
+  BAYTAlVTMRMwEQYDVQQKDApBcHBsZSBJbmMuMSwwKgYDVQQLDCNBcHBsZSBXb3Js
+  ZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9uczFEMEIGA1UEAww7QXBwbGUgV29ybGR3
+  aWRlIERldmVsb3BlciBSZWxhdGlvbnMgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkw
+  HhcNMTUwNzE3MjAxNDQxWhcNMTYwNzE2MjAxNDQxWjB+MR0wGwYKCZImiZPyLGQB
+  AQwNY29tLm9yZy5Jc2FhYzE7MDkGA1UEAwwyQXBwbGUgRGV2ZWxvcG1lbnQgSU9T
+  IFB1c2ggU2VydmljZXM6IGNvbS5vcmcuSXNhYWMxEzARBgNVBAsMCjMyOFNLVDVZ
+  V0MxCzAJBgNVBAYTAlVTMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
+  mVhRtOxnb/D3qwZYJ27yjH3fnbbi/X3MPJ0tiq/82sTlsSkzOhuNFIJZcad5wkCY
+  sPmzKdPK4DZ5WLcNdsfrysG1V23/tZjEZsJPh3bNKBHAWss3ut5mz83cu+kR3z65
+  KK4A2EO6mGtCjlF1JOKiz2XdBrOUmG/SH86xFO9vaUsC2zFpesMpqQ4/g8a8IkL0
+  99EsKmfqgSPl5jih33qQURPE18nhA7x7D7s6A4EmQP2qON9B+Mj3mkoilZXFZBYh
+  FHLm6HkXTRDicRYuTVfY1/eew0dWXQMaMoLSx/eqbJqaukP6RT72MCqKot+rcfw+
+  zYQhuSwZrvkKkYOEI3M78QIDAQABo4IB5TCCAeEwHQYDVR0OBBYEFPZxn8Npnvco
+  N9Bicwq6cSRjDnHfMAkGA1UdEwQCMAAwHwYDVR0jBBgwFoAUiCcXCam2GGCL7Ou6
+  9kdZxVJUo7cwggEPBgNVHSAEggEGMIIBAjCB/wYJKoZIhvdjZAUBMIHxMIHDBggr
+  BgEFBQcCAjCBtgyBs1JlbGlhbmNlIG9uIHRoaXMgY2VydGlmaWNhdGUgYnkgYW55
+  IHBhcnR5IGFzc3VtZXMgYWNjZXB0YW5jZSBvZiB0aGUgdGhlbiBhcHBsaWNhYmxl
+  IHN0YW5kYXJkIHRlcm1zIGFuZCBjb25kaXRpb25zIG9mIHVzZSwgY2VydGlmaWNh
+  dGUgcG9saWN5IGFuZCBjZXJ0aWZpY2F0aW9uIHByYWN0aWNlIHN0YXRlbWVudHMu
+  MCkGCCsGAQUFBwIBFh1odHRwOi8vd3d3LmFwcGxlLmNvbS9hcHBsZWNhLzBNBgNV
+  HR8ERjBEMEKgQKA+hjxodHRwOi8vZGV2ZWxvcGVyLmFwcGxlLmNvbS9jZXJ0aWZp
+  Y2F0aW9uYXV0aG9yaXR5L3d3ZHJjYS5jcmwwCwYDVR0PBAQDAgeAMBMGA1UdJQQM
+  MAoGCCsGAQUFBwMCMBAGCiqGSIb3Y2QGAwEEAgUAMA0GCSqGSIb3DQEBBQUAA4IB
+  AQCce5cdJdKdpCG/pO5iyBKZi4jVy1vMl1MO5085JG2ufrXfudauQIIVntQBn/RT
+  GULzCgNHrrA2U5PLjGLgi5pSVkmg7rWE6V706itenEFRJkhxXOZGkpqw7AML9AQZ
+  YxpwyelSvLdbXIDbMTkQVzIWah27PtCCbGJm+2wccD1Gewn8a1BprxHVaoXoRYc4
+  v+0Rs3CoF3sET2ull3NPG9FFyuVszuQI7RC6doVXL/Vs7Q7pbEFkJnjuTvaQGkAu
+  C028kDYQ5dY4+vinvlVpcCZjgZwvB+yLuwh12Fku2+GSt2dCy7uE5k08MhTOFLjh
+  YOx9ZvBsaI3LcKl58Pb95KWX
+  -----END CERTIFICATE-----
+  Bag Attributes
+      friendlyName: Apple Development IOS Push Services: com.org.Isaac
+      localKeyID: F6 71 9F C3 69 9E F7 28 37 D0 62 73 0A BA 71 24 63 0E 71 DF
+  subject=/UID=com.org.Isaac/CN=Apple Development IOS Push Services: com.org.Isaac/OU=328SKT5YWC/C=US
+  issuer=/C=US/O=Apple Inc./OU=Apple Worldwide Developer Relations/CN=Apple Worldwide Developer Relations Certification Authority
+  -----BEGIN CERTIFICATE-----
+  MIIFfjCCBGagAwIBAgIICORl7oRebFEwDQYJKoZIhvcNAQEFBQAwgZYxCzAJBgNV
+  BAYTAlVTMRMwEQYDVQQKDApBcHBsZSBJbmMuMSwwKgYDVQQLDCNBcHBsZSBXb3Js
+  ZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9uczFEMEIGA1UEAww7QXBwbGUgV29ybGR3
+  aWRlIERldmVsb3BlciBSZWxhdGlvbnMgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkw
+  HhcNMTUwNzE3MjAxNDQxWhcNMTYwNzE2MjAxNDQxWjB+MR0wGwYKCZImiZPyLGQB
+  AQwNY29tLm9yZy5Jc2FhYzE7MDkGA1UEAwwyQXBwbGUgRGV2ZWxvcG1lbnQgSU9T
+  IFB1c2ggU2VydmljZXM6IGNvbS5vcmcuSXNhYWMxEzARBgNVBAsMCjMyOFNLVDVZ
+  V0MxCzAJBgNVBAYTAlVTMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
+  mVhRtOxnb/D3qwZYJ27yjH3fnbbi/X3MPJ0tiq/82sTlsSkzOhuNFIJZcad5wkCY
+  sPmzKdPK4DZ5WLcNdsfrysG1V23/tZjEZsJPh3bNKBHAWss3ut5mz83cu+kR3z65
+  KK4A2EO6mGtCjlF1JOKiz2XdBrOUmG/SH86xFO9vaUsC2zFpesMpqQ4/g8a8IkL0
+  99EsKmfqgSPl5jih33qQURPE18nhA7x7D7s6A4EmQP2qON9B+Mj3mkoilZXFZBYh
+  FHLm6HkXTRDicRYuTVfY1/eew0dWXQMaMoLSx/eqbJqaukP6RT72MCqKot+rcfw+
+  zYQhuSwZrvkKkYOEI3M78QIDAQABo4IB5TCCAeEwHQYDVR0OBBYEFPZxn8Npnvco
+  N9Bicwq6cSRjDnHfMAkGA1UdEwQCMAAwHwYDVR0jBBgwFoAUiCcXCam2GGCL7Ou6
+  9kdZxVJUo7cwggEPBgNVHSAEggEGMIIBAjCB/wYJKoZIhvdjZAUBMIHxMIHDBggr
+  BgEFBQcCAjCBtgyBs1JlbGlhbmNlIG9uIHRoaXMgY2VydGlmaWNhdGUgYnkgYW55
+  IHBhcnR5IGFzc3VtZXMgYWNjZXB0YW5jZSBvZiB0aGUgdGhlbiBhcHBsaWNhYmxl
+  IHN0YW5kYXJkIHRlcm1zIGFuZCBjb25kaXRpb25zIG9mIHVzZSwgY2VydGlmaWNh
+  dGUgcG9saWN5IGFuZCBjZXJ0aWZpY2F0aW9uIHByYWN0aWNlIHN0YXRlbWVudHMu
+  MCkGCCsGAQUFBwIBFh1odHRwOi8vd3d3LmFwcGxlLmNvbS9hcHBsZWNhLzBNBgNV
+  HR8ERjBEMEKgQKA+hjxodHRwOi8vZGV2ZWxvcGVyLmFwcGxlLmNvbS9jZXJ0aWZp
+  Y2F0aW9uYXV0aG9yaXR5L3d3ZHJjYS5jcmwwCwYDVR0PBAQDAgeAMBMGA1UdJQQM
+  MAoGCCsGAQUFBwMCMBAGCiqGSIb3Y2QGAwEEAgUAMA0GCSqGSIb3DQEBBQUAA4IB
+  AQCce5cdJdKdpCG/pO5iyBKZi4jVy1vMl1MO5085JG2ufrXfudauQIIVntQBn/RT
+  GULzCgNHrrA2U5PLjGLgi5pSVkmg7rWE6V706itenEFRJkhxXOZGkpqw7AML9AQZ
+  YxpwyelSvLdbXIDbMTkQVzIWah27PtCCbGJm+2wccD1Gewn8a1BprxHVaoXoRYc4
+  v+0Rs3CoF3sET2ull3NPG9FFyuVszuQI7RC6doVXL/Vs7Q7pbEFkJnjuTvaQGkAu
+  C028kDYQ5dY4+vinvlVpcCZjgZwvB+yLuwh12Fku2+GSt2dCy7uE5k08MhTOFLjh
+  YOx9ZvBsaI3LcKl58Pb95KWX
+  -----END CERTIFICATE-----
+  Bag Attributes
+      friendlyName: isaac.moldofsky@gmail.com
+      localKeyID: F6 71 9F C3 69 9E F7 28 37 D0 62 73 0A BA 71 24 63 0E 71 DF
+  Key Attributes: <No Attributes>
+  -----BEGIN RSA PRIVATE KEY-----
+  MIIEowIBAAKCAQEAmVhRtOxnb/D3qwZYJ27yjH3fnbbi/X3MPJ0tiq/82sTlsSkz
+  OhuNFIJZcad5wkCYsPmzKdPK4DZ5WLcNdsfrysG1V23/tZjEZsJPh3bNKBHAWss3
+  ut5mz83cu+kR3z65KK4A2EO6mGtCjlF1JOKiz2XdBrOUmG/SH86xFO9vaUsC2zFp
+  esMpqQ4/g8a8IkL099EsKmfqgSPl5jih33qQURPE18nhA7x7D7s6A4EmQP2qON9B
+  +Mj3mkoilZXFZBYhFHLm6HkXTRDicRYuTVfY1/eew0dWXQMaMoLSx/eqbJqaukP6
+  RT72MCqKot+rcfw+zYQhuSwZrvkKkYOEI3M78QIDAQABAoIBAHobI+Tnom42+WCM
+  SsILzMQmr1vM9+9Wrr1Ng6g9/yDNTQHHhu0sZyj/qu2fqIsQGQZDr3ENHy1u8y27
+  hdMh2xa3LQmTo90c6rfQ3rdF2JOhnwQtchExa7jpem1/aCXWsmY8OJv9QqaAMp1V
+  K+zVM0PnHxtpAFhqIm4FjmjXSGYUg4Jb/zkhtkI7KRQsqKa4cxadl6wvTKF2JFxO
+  ++0moduG7H5JhO/uKqR0qCUrPrM0dY5wt0jK64KfW7rcDW5OHxGUQKtYBsVxHxzp
+  YyrOJaiGPpkMtXA2ldw8Xm/Lf7Uerk3r5HHKrtgpyPchkpWi7MvTcUwyAHEy1YVf
+  go4ecskCgYEAx6KJuFpSuE91JpCZVPLiZi3q21mol3souo2OkTK9bP6JJ8RECzT4
+  2oovJXV8ZCntt2A/L2vCRx0/KS58vGodE41xFgUcAqmaxGSY/pliGUv5Jz9wFx2W
+  AaJBo09+u17Cm65WI7f9Gu/YeHXK38NqY0qsgdIUH75QRkF1oyXy9PMCgYEAxKP7
+  DHjSjm4lLuQFQxBUX5UaXyuLQMVkIQDWw19sx8tU1sxxDKJztpBm0yKtw55P2CVY
+  sEQef9Zv3pQGEaDIX5kuNbVlNqLun7iuM7b5riEBP2i08fioqxjzdXtE1Gd5xg+d
+  9uaAwIWS4xuicHZp5RfDYP73p9NO39duljtV1IsCgYEAluP8klH62gJ8urRqoLGg
+  e7jTHg5Lhot5QmACVS7zzDEre9o1z/6u5YeykO6XIaIrYgImX/Jj+ppZhgf0Eflm
+  lsO+qPUdscl+CFk85psKcbJh1M93KpGbMwrv778DPB4om8EOrJrBfR2yEYJ+39h1
+  Ti0/7DPcMA4J4MBTaQgi9g8CgYAIdtqGlejSUrFtDEmhsmE/YAarlA9BcX224fdc
+  n43qJ7l2KYSwO4npkNusOPDr27OqJSllJPl/HTbhiNOeDKKzYr+XdkBuEqNc4rE0
+  7qpesXMBTLuFMuPnwYIxS2YSEoYuXVu6Vf+yyc0h032xg5dZUiWJ1k/IvJLHldlf
+  xBwUswKBgGkDxcUilOurZovlBjRcHE7QgiCB+G1TI09xiGiTcoNgytmN4Z3rLI37
+  MlM78t/YtdVwLIxfYm2cGHtnc7nxIvyNWaYS3FuyI7+2hy2FoHrD2KGXNdFQhsrG
+  xkNyeGrMGzxRuVPN2tyMNzZIJTRBqBc3swNrXdjRw5qvpR6nUuVM
+  -----END RSA PRIVATE KEY-----
+  Bag Attributes
+      friendlyName: isaac.moldofsky@gmail.com
+      localKeyID: F6 71 9F C3 69 9E F7 28 37 D0 62 73 0A BA 71 24 63 0E 71 DF
+  Key Attributes: <No Attributes>
+  -----BEGIN RSA PRIVATE KEY-----
+  MIIEowIBAAKCAQEAmVhRtOxnb/D3qwZYJ27yjH3fnbbi/X3MPJ0tiq/82sTlsSkz
+  OhuNFIJZcad5wkCYsPmzKdPK4DZ5WLcNdsfrysG1V23/tZjEZsJPh3bNKBHAWss3
+  ut5mz83cu+kR3z65KK4A2EO6mGtCjlF1JOKiz2XdBrOUmG/SH86xFO9vaUsC2zFp
+  esMpqQ4/g8a8IkL099EsKmfqgSPl5jih33qQURPE18nhA7x7D7s6A4EmQP2qON9B
+  +Mj3mkoilZXFZBYhFHLm6HkXTRDicRYuTVfY1/eew0dWXQMaMoLSx/eqbJqaukP6
+  RT72MCqKot+rcfw+zYQhuSwZrvkKkYOEI3M78QIDAQABAoIBAHobI+Tnom42+WCM
+  SsILzMQmr1vM9+9Wrr1Ng6g9/yDNTQHHhu0sZyj/qu2fqIsQGQZDr3ENHy1u8y27
+  hdMh2xa3LQmTo90c6rfQ3rdF2JOhnwQtchExa7jpem1/aCXWsmY8OJv9QqaAMp1V
+  K+zVM0PnHxtpAFhqIm4FjmjXSGYUg4Jb/zkhtkI7KRQsqKa4cxadl6wvTKF2JFxO
+  ++0moduG7H5JhO/uKqR0qCUrPrM0dY5wt0jK64KfW7rcDW5OHxGUQKtYBsVxHxzp
+  YyrOJaiGPpkMtXA2ldw8Xm/Lf7Uerk3r5HHKrtgpyPchkpWi7MvTcUwyAHEy1YVf
+  go4ecskCgYEAx6KJuFpSuE91JpCZVPLiZi3q21mol3souo2OkTK9bP6JJ8RECzT4
+  2oovJXV8ZCntt2A/L2vCRx0/KS58vGodE41xFgUcAqmaxGSY/pliGUv5Jz9wFx2W
+  AaJBo09+u17Cm65WI7f9Gu/YeHXK38NqY0qsgdIUH75QRkF1oyXy9PMCgYEAxKP7
+  DHjSjm4lLuQFQxBUX5UaXyuLQMVkIQDWw19sx8tU1sxxDKJztpBm0yKtw55P2CVY
+  sEQef9Zv3pQGEaDIX5kuNbVlNqLun7iuM7b5riEBP2i08fioqxjzdXtE1Gd5xg+d
+  9uaAwIWS4xuicHZp5RfDYP73p9NO39duljtV1IsCgYEAluP8klH62gJ8urRqoLGg
+  e7jTHg5Lhot5QmACVS7zzDEre9o1z/6u5YeykO6XIaIrYgImX/Jj+ppZhgf0Eflm
+  lsO+qPUdscl+CFk85psKcbJh1M93KpGbMwrv778DPB4om8EOrJrBfR2yEYJ+39h1
+  Ti0/7DPcMA4J4MBTaQgi9g8CgYAIdtqGlejSUrFtDEmhsmE/YAarlA9BcX224fdc
+  n43qJ7l2KYSwO4npkNusOPDr27OqJSllJPl/HTbhiNOeDKKzYr+XdkBuEqNc4rE0
+  7qpesXMBTLuFMuPnwYIxS2YSEoYuXVu6Vf+yyc0h032xg5dZUiWJ1k/IvJLHldlf
+  xBwUswKBgGkDxcUilOurZovlBjRcHE7QgiCB+G1TI09xiGiTcoNgytmN4Z3rLI37
+  MlM78t/YtdVwLIxfYm2cGHtnc7nxIvyNWaYS3FuyI7+2hy2FoHrD2KGXNdFQhsrG
+  xkNyeGrMGzxRuVPN2tyMNzZIJTRBqBc3swNrXdjRw5qvpR6nUuVM
+  -----END RSA PRIVATE KEY-----
+  "
+    notification = Houston::Notification.new(device: recipient_token)
+    notification.alert = "#{first_name} #{last_name} is now within 1000 feet of you!"
+    notification.badge = 17
+    notification.sound = "sosumi.aiff"
+    notification.category = "INVITE_CATEGORY"
+    notification.content_available = true
+    notification.custom_data = {foo: "bar"}
+    APN.push(notification)
   end
 end
