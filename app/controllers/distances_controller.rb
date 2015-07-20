9aeb83a98d47f4dc8567b59670b1548e2b2e3e58
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-07-20 16:26:35 -0500

    logging to database

--- app/controllers/distances_controller.rb
+++ app/controllers/distances_controller.rb
@@ -46,7 +46,7 @@ class DistancesController < ApplicationController
     @user.lon = params[:lon].to_f
     @user.lat_lon_log += "#{@user.lat}, #{@user.lon};"
     @user.save
-    puts "**************************** #{@user.lat_lon_log.split(';')}"
+    puts "**************************** #{@user.lat_lon_log}"
     @nearby_friends = @user.nearby_friends_images.split(",")
     if @nearby_friends.empty?
       @nearby_friends << "http://www.rollitup.org/proxy.php?image=http%3A%2F%2Fwww.esreality.com%2Ffiles%2Fplaceimages%2F2013%2F99064-yo-dawg-i-heard-you-have-no-friends-30.jpeg&hash=b7655b7718dfb6c45f7bbee04ed90d00"
