57631a8b9c32082a7a86f30f5a4be69c7eb3ae4b
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-07-21 13:53:02 -0500

    whenever is running map every minute

--- config/initializers/houston.rb
+++ config/initializers/houston.rb
@@ -1,5 +1,6 @@
 require 'houston'
 
+
 # Environment variables are automatically read, or can be overridden by any specified options. You can also
 # conveniently use `Houston::Client.development` or `Houston::Client.production`.
 APN = Houston::Client.development
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,20 @@
+# require 'whenever'
+
 # Use this file to easily define all of your cron jobs.
 #
+set :environment, "development"
+set :output, {:error => "log/error.log", :standard => "log/cron.log"}
+
 # It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-every 120.seconds do
+puts "before SCHEDULE"
+every 1.minute do
   rake "map"
+puts "inside SCHEDULE"
+
 end
+puts "after SCHEDULE"
+
+# http://en.wikipedia.org/wiki/Cron
 # Example:
 #
 # set :output, "/path/to/my/cron_log.log"
