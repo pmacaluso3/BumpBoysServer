74be602e25905c744fb27d2943e4f74ba74007dc
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-07-22 09:35:59 -0500

    removing protect from forgery

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
+
   def create
     @params = params
     puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ params from session post was #{params.inspect}"
