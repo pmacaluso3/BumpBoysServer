1d45706bc880a3dbe194cca48a9a38360137ccaa
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-07-22 14:56:22 -0500

    trying to handle a batch of contacts at once, and to destroy all of a user's previous contacts first

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,37 +1,33 @@
 class ContactsController < ApplicationController
   def create
-    @params = params
     @user = User.find_by("<#{user_token}>")
-    @contact = Contact.create(contact_params_without_user_token)
-    @contact.user_id = @user.id
-    puts "********************** here's the new contact: #{@contact.inspect}"
-    puts "********************** params[:user_token] was #{params[:user_token]}"
-    puts "********************** params from a contacts request was #{params.inspect}"
-    if @contact.save
-      puts "@@@@@@@@@@@@@@@@@@@@@@@@ the contact saved"
-    else
-      puts "@@@@@@@@@@@@@@@@@@@@@@@@ the contact didn't save"
+    @user.contacts.each {|c|c.destroy}
+    puts "************************************ #{params.inspect}"
+    new_contacts = []
+    params.each do |key,value|
+      if key =~ /\Aperson/
+        new_contacts << value
+      end
     end
+    new_contacts.each do |contact|
+      contact[:last_name] = "" if contact[:last_name] == "null"
+      Contact.create(first_name: contact[:first_name], last_name: contact[:last_name], phone_number: contact[:number], user_id: @user.id)
+    end
+    # @contact = Contact.create(contact_params_without_user_token)
+    # @contact.user_id = @user.id
+    # puts "********************** here's the new contact: #{@contact.inspect}"
+    # puts "********************** params[:user_token] was #{params[:user_token]}"
+    # puts "********************** params from a contacts request was #{params.inspect}"
+    # if @contact.save
+    #   puts "@@@@@@@@@@@@@@@@@@@@@@@@ the contact saved"
+    # else
+    #   puts "@@@@@@@@@@@@@@@@@@@@@@@@ the contact didn't save"
+    # end
     respond_to do |format|
       format.html {render 'contacts/create'}
       format.json {render json: "You made it to create contact"}
     end
 
-  end
-
-
-  private
-
-  def contact_params
-    params.require(:person).permit(:first_name, :last_name, :phone_number, :user_token)
-  end
-
-  def contact_params_without_user_token
-    params.require(:person).permit(:first_name, :last_name, :phone_number)
-  end
-
-  def user_token
-    params.require(:person).permit(:user_token)
   end
 
 end
