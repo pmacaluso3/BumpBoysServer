6aece8f01bd227f30eb53b804d8a58291556bdd4
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-07-21 20:22:14 -0500

    figuring our herokku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     @params = params
-    puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ params from session post was #{params.inspect}"
+    puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ params from session post was #{params.inspect}"
     respond_to do |format|
       format.html {render 'sessions/create'}
       format.json {render json: "You made it to create session"}
